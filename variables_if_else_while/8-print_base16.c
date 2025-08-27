c820b2c9781d0afd7eb872fb538e2570bb3e3995
root <abdullah.m122144@gamil.com>
2025-08-27 16:33:30 +0000

    Add task 8: print hexadecimal numbers using putchar

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main  base 16 (0-9 «Ù… a-f)
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	char ch;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+	}
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
