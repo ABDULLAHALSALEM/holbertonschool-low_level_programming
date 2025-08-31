e1dee93518bb0f10353acbf5ebb9c87aade530cc
ABDULLAHALSALEM <abdullah.m122144@gmail.com>
2025-08-31 11:24:56 +0000

    Implement 1-isdigit: return 1 for '0'..'9', else 0

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if a character code is a digit (0..9)
+ * @c: int representing the character code to check
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
