06da83410b8bed98b4bc88fe555d2ae6fbfe615f
ABDULLAHALSALEM <abdullah.m122144@gmail.com>
2025-09-10 16:19:32 +0000

    Task 3: multiply two numbers; print Error on wrong argc

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers given as command-line arguments
+ * @argc: number of arguments
+ * @argv: array of argument strings
+ *
+ * Return: 0 on success; 1 if the program does not receive two arguments.
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, product;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	product = a * b;
+
+	printf("%d\n", product);
+	return (0);
+}
