5b043e5ce0b9bf343195b772026018d2ab8b2410
ABDULLAHALSALEM <abdullah.m122144@gmail.com>
2025-09-15 15:09:48 +0000

    Complete malloc/free task: create_array + headers

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -11,21 +11,21 @@
  */
 char *_strdup(char *str)
 {
-    char *dup;
-    unsigned int len = 0, i;
+	char *dup;
+	unsigned int len = 0, i;
 
-    if (str == NULL)
-        return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-    while (str[len] != '\0')
-        len++;
+	while (str[len] != '\0')
+		len++;
 
-    dup = malloc((len + 1) * sizeof(*dup));
-    if (dup == NULL)
-        return (NULL);
+	dup = malloc((len + 1) * sizeof(*dup));
+	if (dup == NULL)
+		return (NULL);
 
-    for (i = 0; i <= len; i++)
-        dup[i] = str[i];
+	for (i = 0; i <= len; i++)
+		dup[i] = str[i];
 
-    return (dup);
+	return (dup);
 }
