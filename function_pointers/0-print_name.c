cefa081d90ecef4eab130f9457427fee918c0a21
ABDULLAHALSALEM <abdullah.m122144@gamil.com>
2025-09-21 13:51:18 +0000

    Betty: wrap long comment lines under 80 chars in 0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,15 +1,15 @@
 #include "function_pointers.h"
 
 /**
- * print_name - يستدعي دالة الطباعة الممرّرة لطباعة الاسم.
- * @name: الاسم المراد طباعته.
- * @f: مؤشّر لدالة تستقبل (char *) ولا تعيد قيمة.
+ * print_name - Call a user-supplied printer for a name.
+ * @name: The name to print.
+ * @f: Pointer to a function that takes (char *) and returns void.
  *
- * Return: لا شيء.
+ * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
+	if (name == 0 || f == 0)
 		return;
 
 	f(name);
