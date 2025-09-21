2f9353425fd1ba5008479e69d84eb53e3e767251
ABDULLAHALSALEM <abdullah.m122144@gamil.com>
2025-09-21 13:54:08 +0000

    Fix Betty style for 0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,15 +1,15 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Call a user-supplied printer for a name.
- * @name: The name to print.
- * @f: Pointer to a function that takes (char *) and returns void.
+ * print_name - call a function to print a name
+ * @name: pointer to the name string
+ * @f: function pointer that takes a char * and returns void
  *
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == 0 || f == 0)
+	if (name == NULL || f == NULL)
 		return;
 
 	f(name);
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,10 +1,7 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-/* منصّة التصحيح توفّر _putchar؛ استخدمه فقط إذا احتجته في ملفات أخرى */
-int _putchar(char c);
-
-/* بروتوتايب المطلوب في هذا التمرين */
 void print_name(char *name, void (*f)(char *));
+int _putchar(char c);
 
 #endif /* FUNCTION_POINTERS_H */
