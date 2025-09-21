d26c24f6852a6d35644ce6d4839e1e225415ba3e
ABDULLAHALSALEM <abdullah.m122144@gamil.com>
2025-09-21 14:08:44 +0000

    Fix Betty-doc: remove bogus struct doc from header

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,12 +1,6 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-/**
- * struct function_pointers - header for task 0
- * prototypes used in this project
- */
-
 void print_name(char *name, void (*f)(char *));
-int _putchar(char c);
 
 #endif /* FUNCTION_POINTERS_H */
