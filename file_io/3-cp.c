5ebe029179c75a12b28b5b1e05230d468e6fe805
ABDULLAHALSALEM <Abdullah.m122144@gmail.com>
2025-10-16 09:16:39 +0000

    ff

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,22 +1,40 @@
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
+#include <stdio.h>      /* dprintf */
+#include <fcntl.h>      /* open */
+#include <unistd.h>     /* read, write, close */
+#include <stdlib.h>     /* malloc, free, exit */
 #include <sys/types.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <sys/stat.h>
 
-#define BUF_SIZE 1024
+#define BUFSIZE 1024
 
 /**
- * close_fd - Close a file descriptor and handle errors.
- * @fd: File descriptor to close.
+ * create_buffer - Allocate a 1KB buffer for copying
+ * @file_to: destination filename (for error messages)
+ *
+ * Return: pointer to allocated buffer
+ * Description: On allocation failure, prints an error to STDERR and exits 99.
+ */
+static char *create_buffer(const char *file_to)
+{
+	char *buf = malloc(BUFSIZE);
+
+	if (buf == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+	return (buf);
+}
+
+/**
+ * close_fd - Close a file descriptor with error handling
+ * @fd: file descriptor to close
+ *
+ * Description: On failure, prints an error to STDERR and exits 100.
  */
 static void close_fd(int fd)
 {
-	int c;
-
-	c = close(fd);
-	if (c == -1)
+	if (close(fd) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
@@ -24,65 +42,123 @@ static void close_fd(int fd)
 }
 
 /**
- * main - Copy the content of a file to another file.
- * @ac: Argument count.
- * @av: Argument vector.
+ * write_chunk - Write exactly @r bytes from @buf to @fd_to
+ * @fd_to: destination file descriptor
+ * @buf: buffer holding data to write
+ * @r: number of bytes to write
+ * @file_to: destination filename (for error messages)
  *
- * Return: 0 on success, exits with codes 97-100 on failure.
+ * Description: Loops until all bytes are written. On write failure, exits 99.
  */
-int main(int ac, char **av)
+static void write_chunk(int fd_to, char *buf, ssize_t r, const char *file_to)
 {
-	int fd_from, fd_to;
-	ssize_t rcount, wcount, written;
-	char buf[BUF_SIZE];
+	ssize_t total = 0;
+	ssize_t w;
 
-	if (ac != 3)
+	while (total < r)
+	{
+		w = write(fd_to, buf + total, r - total);
+		if (w == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
+				file_to);
+			exit(99);
+		}
+		total += w;
+	}
+}
+
+/**
+ * copy_rest - Open target, write first block, then copy remaining blocks
+ * @fd_from: source file descriptor
+ * @file_to: destination filename
+ * @buf: copy buffer
+ * @r_first: number of bytes already read from source
+ * @file_from: source filename (for error messages)
+ *
+ * Description: Creates/truncates @file_to with mode 0664. Writes the first
+ * block if any, then continues reading/writing in 1KB blocks. On read
+ * failure exits 98; on create/write failure exits 99. Closes @fd_to.
+ */
+static void copy_rest(int fd_from, const char *file_to, char *buf,
+		      ssize_t r_first, const char *file_from)
+{
+	int fd_to;
+	ssize_t r;
+
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+
+	if (r_first > 0)
+		write_chunk(fd_to, buf, r_first, file_to);
+
+	r = read(fd_from, buf, BUFSIZE);
+	while (r > 0)
+	{
+		write_chunk(fd_to, buf, r, file_to);
+		r = read(fd_from, buf, BUFSIZE);
+	}
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+			file_from);
+		close_fd(fd_to);
+		exit(98);
+	}
+
+	close_fd(fd_to);
+}
+
+/**
+ * main - Copy the content of a file to another file
+ * @argc: argument count
+ * @argv: argument vector (argv[1]=file_from, argv[2]=file_to)
+ *
+ * Return: 0 on success
+ * Description: Exits with 97 (usage), 98 (read), 99 (write/create),
+ * and 100 (close) on errors. Reads first to detect read errors before
+ * touching the destination, as required by tests.
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from;
+	ssize_t r_first;
+	char *buf;
+
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	fd_from = open(av[1], O_RDONLY);
+	buf = create_buffer(argv[2]);
+
+	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+			argv[1]);
+		free(buf);
 		exit(98);
 	}
 
-	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
+	r_first = read(fd_from, buf, BUFSIZE);
+	if (r_first == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+			argv[1]);
+		free(buf);
 		close_fd(fd_from);
-		exit(99);
-	}
-
-	while ((rcount = read(fd_from, buf, BUF_SIZE)) > 0)
-	{
-		written = 0;
-		while (written < rcount)
-		{
-			wcount = write(fd_to, buf + written, rcount - written);
-			if (wcount == -1)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-				close_fd(fd_from);
-				close_fd(fd_to);
-				exit(99);
-			}
-			written += wcount;
-		}
-	}
-
-	if (rcount == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		close_fd(fd_from);
-		close_fd(fd_to);
 		exit(98);
 	}
 
+	copy_rest(fd_from, argv[2], buf, r_first, argv[1]);
+
+	free(buf);
 	close_fd(fd_from);
-	close_fd(fd_to);
-	return (0);
+	return (0);
 }
